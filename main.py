1453e77ca310940469157dbafb276aed1fb87dfa
smy-0319 <1611646963@qq.com>
2025-10-17 11:21:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-# 这个文件用于编写代码
+s = input("请输入一行字符：")
+letter = 0
+digit = 0
+space = 0
+other = 0
+for c in s:
+    if c.isalpha():
+        letter += 1
+    elif c.isdigit():
+        digit += 1
+    elif c.isspace():
+        space += 1
+    else:
+        other += 1
+print(f"英文字符: {letter}")
+print(f"数字: {digit}")
+print(f"空格: {space}")
+print(f"其他字符: {other}")
